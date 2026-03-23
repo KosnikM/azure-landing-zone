7159841c6d8c3a091cf7ab2a9cf8dadcb08d8e1b
KosnikM <mkosnikk@gmail.com>
2026-03-23 05:24:43 +0100

    feat: add governance module - allowed VM SKUs policy

--- modules/governance/main.tf
+++ modules/governance/main.tf
@@ -8,24 +8,3 @@ resource "azurerm_resource_group_policy_assignment" "allowed_vm_skus" {
   })
 }
 
-
-resource "azurerm_storage_management_policy" "lifecycle" {
-  storage_account_id = azurerm_storage_account.this.id
-
-  rule {
-    name    = "move-to-cool"
-    enabled = true
-
-    filters {
-      blob_types = ["blockBlob"]
-    }
-
-    actions {
-      base_blob {
-        tier_to_cool_after_days_since_modification_greater_than    = 30
-        tier_to_archive_after_days_since_creation_greater_than = 90
-        delete_after_days_since_modification_greater_than          = 365
-      }
-    }
-  }
-}
